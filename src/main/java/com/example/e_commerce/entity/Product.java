a15449a5931e8b43fe2c166770c7cec614dd83af
Samet Ruzgar <sametruzgar.2804@gmail.com>
2025-11-06 05:22:32 +0300

    entity start

---
+++ src/main/java/com/example/e_commerce/entity/Product.java
@@ -0,0 +1,4 @@
+package com.example.e_commerce.entity;
+
+public class Product {
+}
---
+++ src/main/java/com/example/e_commerce/entity/User.java
@@ -0,0 +1,66 @@
+package com.example.e_commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated
+    @Column(nullable = false)
+    private UserStatus status=UserStatus.ACTIVE;
+
+    @OneToMany (mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Adress> adresses=new ArrayList<>();
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,FetchType.LAZY)
+    private Cart cart;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,FetchType.LAZY)
+    private List<Order> orders=new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public enum Role{
+        ADMIN,USER
+    }
+
+    public enum UserStatus{
+        ACTIVE,FROZEN,BANNED
+    }
+}
